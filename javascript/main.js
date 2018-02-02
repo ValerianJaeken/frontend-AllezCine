837c5bdccf100a197ea7d86984b05ccf0d591c56
ValerianJaeken <valerianrj@gmail.com>
2018-02-02 11:52:42 +0100

    Added Cancer class
    Cancer

--- javascript/main.js
+++ javascript/main.js
@@ -18,3 +18,17 @@ $(".modal").css("display","none")
 $(".footerUp").click(function(){
   $('html, body').animate({ scrollTop: 0 });
 })
+
+$(".cancer").click(function(){
+  let title=$(this).attr("data-title");
+  let date=$(this).attr("data-date");
+  let genre=$(this).attr("data-genre");
+  let trailer=$(this).attr("data-trailer");
+  $(".movieCancer").html('<div class="cancerTrailer"><iframe width=450 height=250 src="' + trailer + '" frameborder="0" encrypted-media allowfullscreen></iframe></div>');
+  // APPEND MAKES IT SO THE TEXT IS INCREMENTAL INSTEAD OF REPLACING THE FORMER HTML EVERYTIME IF YOU USE ANOTHER HTML TAG
+  $(".movieCancer").append("<div class=cancerTitle>Title : " + title + "</div>");
+  $(".movieCancer").append("<div class=cancerDate>Release date : "+ date +"</div>");
+  $(".movieCancer").append("<div class=cancerGenre>Genre : "+ genre +"</div>");
+  console.log(date,genre);
+
+})
