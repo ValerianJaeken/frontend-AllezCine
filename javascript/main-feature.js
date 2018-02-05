2d562f356b2a4020e193a1c1115cc795d4a53786
PrinceKombo <princekombo@gmail.com>
2018-02-05 10:07:12 +0100

    jqwery
    comment

--- javascript/main-feature.js
+++ javascript/main-feature.js
@@ -1,8 +1,8 @@
-console.log($('.featured-movies .show-more'));
-var fm = $('.featured-movies .show-more'); // fm = featured movies
-$(document).ready(function(){
-
-$('#show').on('click',function(){
-fm.eq(2).toggle();
-})
-});
+// console.log($('.featured-movies .show-more'));
+// var fm = $('.featured-movies .show-more'); // fm = featured movies
+// $(document).ready(function(){
+//
+// $('#show').on('click',function(){
+// fm.eq(2).toggle();
+// })
+// });
