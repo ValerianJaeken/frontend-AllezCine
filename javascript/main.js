844788aedad4146720f901ee30db7b7bda2473de
ValerianJaeken <valerianrj@gmail.com>
2018-02-05 12:03:33 +0100

    adad
    dad

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-  //header
-$("#header_title").text("latest" "" "" "" "")
-
+// $(document).ready(function(){
+//   //header
+// $("#header_title").text("latest" "" "" "" "")
+// }
 
 // modal of contact us
 var modal = $("contactForm");
@@ -15,7 +15,7 @@ $(".modal").css("display","none")
 })
 
 
-});
+
 
 $(".footerUp").click(function(){
   $('html, body').animate({ scrollTop: 0 });
@@ -26,7 +26,7 @@ $(".cancer").click(function(){
   let date=$(this).attr("data-date");
   let genre=$(this).attr("data-genre");
   let trailer=$(this).attr("data-trailer");
-  $(".movieCancer").html('<div class="cancerTrailer"><iframe width=450 height=250 src="' + trailer + '" frameborder="0" encrypted-media allowfullscreen></iframe></div>');
+  $(".movieCancer").html('<div class="cancerTrailer"><iframe width=auto height=250 src="' + trailer + '" frameborder="0" encrypted-media allowfullscreen></iframe></div>');
   // APPEND MAKES IT SO THE TEXT IS INCREMENTAL INSTEAD OF REPLACING THE FORMER HTML EVERYTIME IF YOU USE ANOTHER HTML TAG
   $(".movieCancer").append("<div class=cancerTitle>Title : " + title + "</div>");
   $(".movieCancer").append("<div class=cancerDate>Release date : "+ date +"</div>");
